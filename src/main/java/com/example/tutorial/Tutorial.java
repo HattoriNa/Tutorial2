832854e8fd742965c50a7071b8c076b2d90bbaf4
Usuario <figlcam685@g.educaand.es>
2022-09-28 08:23:33 +0200

    Ver 1.0

---
+++ src/main/java/com/example/tutorial/Tutorial.java
@@ -0,0 +1,31 @@
+package com.example.tutorial;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
+
+
+public class Tutorial extends Application  {
+    public static void main(String args[]){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Button button1 = new Button("Button1");
+        Button button2 = new Button("Button2");
+        Button button3 = new Button("Button3a");
+        Button button4 = new Button("Button4");
+
+        TilePane layout = new TilePane(button1, button2, button3, button4);
+
+        Scene scene = new Scene(layout, 350, 250);
+
+        primaryStage.setTitle("Tilepane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
